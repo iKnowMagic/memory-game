2fa3c1c208eb944d104aedd86b1be241bc6bc230
iknowmagic <ilan@iknowmagic.net>
2021-12-26 13:50:14 -0800

    init

--- windi.config.ts
+++ windi.config.ts
@@ -4,9 +4,22 @@ import { defineConfig } from 'windicss/helpers'
 export default defineConfig({
   extend: {
     theme: {
+      screens: {
+        sm: '375px',
+        md: '768px',
+        lg: '1440px'
+      },
       colors: {
         gray: colors.gray,
         dark: colors.dark
+      },
+      fontSize: {
+        base: '18px',
+        h1: '48px',
+        h2: '32px',
+        h3: '20px',
+        'game-sm': '44px',
+        'game-lg': '56px'
       }
     }
   }
